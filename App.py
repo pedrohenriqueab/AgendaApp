a510e23114445adef729c591c20b63321a34e34b
MarinaAraujo <marinamarttinsmap@gmail.com>
2017-08-26 22:24:08 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,28 @@
 import json
+def regulariza (pergunta, inicio, fim):
+  while True:
+    try:
+      valor = int(input(pergunta))
+      if inicio <= valor <= fim:
+        return (valor)
+      return
+    except ValueError:
+      print("valor inválido, digite entre %d e %d" % (inicio, fim))
+def menu():
+  print("""
+  1- Adicionar contato
+  2- Editar contato
+  3-Pesquisar contato
+  4-Visualisar lista de contatos
+  5-Aapagar contato
+  6- Sair""")
+  return regulariza("Escolha uma opção: ", 1, 6)
+while True:
+    opção = menu()
+    if opção == 6
+        break
+
+
 def main():
     while True:
         try:
@@ -12,5 +36,6 @@ def main():
 
         except FileNotFoundError:
             print("arquivo não encontrado")
+               
 if __name__ == '__main__':
     main()
