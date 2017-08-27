e8c5fe9dad0d02a5765b6162de3aa7cb81096433
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2017-08-27 17:22:49 -0300

    Update Telefone.py

--- Telefone.py
+++ Telefone.py
@@ -1,6 +1,6 @@
 import datetime
 class Telefone(object):
-    def __init__(self,codigoPais, ddd, numero):
+    def __init__(self, codigoPais, ddd, numero):
         self.codigoPais = codigoPais
         self.ddd = ddd
         self.numero = numero
