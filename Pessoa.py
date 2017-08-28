780468015772afbac59d1f1b4a8694aacc19f9d4
MarinaAraujo <marinamarttinsmap@gmail.com>
2017-08-28 11:01:48 -0300

    Add files via upload

---
+++ Pessoa.py
@@ -0,0 +1,15 @@
+import datetime
+class Pessoa(object):
+    def main(self, nome, nascimento, email):
+        self.nome = nome
+        self.nascimento = nascimento(datetime.date(2001, 10, 9))
+        self.email = email('proprietario@gmail.com')
+
+
+
+
+
+
+
+
+
