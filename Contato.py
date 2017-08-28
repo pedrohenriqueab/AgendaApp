eaf3230931ce2f569383feb0a34b9a914e62ae0b
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2017-08-28 14:45:38 -0300

    Update Contato.py

--- Contato.py
+++ Contato.py
@@ -1,7 +1,9 @@
 import datetime
 from Model.Agenda import *
-class Contato():
-    def __init__(self, criacao):
+from Model.Pessoa import Pessoa
+class Contato(Pessoa):
+    def __init__(self, criacao, nome, nascimento, email):
+        super(self, Pessoa).__init__(nome, nascimento, email)
         self.criacao = criacao(datetime.datetime.now().strftime("%y-%m-%d-%H-%M"))
 
     def listarTelefone(self):
