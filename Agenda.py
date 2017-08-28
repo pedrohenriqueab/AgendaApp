cc37c6b37069ccef21aced23f9b1ff39dcda3b87
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2017-08-28 11:03:08 -0300

    Update Agenda.py

--- Agenda.py
+++ Agenda.py
@@ -1,11 +1,40 @@
-import datetime
-class Pessoa(object):
-    def main(self, nome, nascimento, email):
-        self.nome = nome
-        self.nascimento = nascimento(datetime.date(2001, 10, 9))
-        self.email = email('proprietario@gmail.com')
-
+import json
+import App
 class Agenda():
-    def main(self, proprietario):
-        self.proprietario = proprietario(Pessoa)
+    def __init__(self, proprietario):
+        self.proprietario = proprietario
+        self.contatos = []
+
+    def AdicionarNovoContato(self, contatos):
+        self.contatos.append(contatos)
+
+    def ListarDados(self):
+        for contatos in self.contatos:
+            print(contatos)
+        def __str__(self):
+            return "%s" % (self.contatos)
+
+    def PesquisarContato(self, nome):
+        nome = nome.lower()
+        for a, b in enumerate (Agenda):
+            if b [0].lower() == nome:
+                return (a)
+
+    def ApagarUmContato(self, nome):
+        global agenda
+        p = self.Pesquisar(nome)
+        if p != None:
+            del agenda[p]
+        while True:
+            try:
+                p == None
+                break
+            except:
+                print("Contato não encontrado.")
+
+    def QuantidadeDeContatos(self):
+        return len(self.contatos)
+
+    def SalvarContato(self):
+        pass
 
