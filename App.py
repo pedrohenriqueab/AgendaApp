5a6a175d71efa5c4358471bf9355978627788c50
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2017-08-28 11:04:04 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,41 +1,48 @@
-import json
-def regulariza (pergunta, inicio, fim):
-  while True:
-    try:
-      valor = int(input(pergunta))
-      if inicio <= valor <= fim:
-        return (valor)
-      return
-    except ValueError:
-      print("valor inválido, digite entre %d e %d" % (inicio, fim))
+from Model.Agenda import Agenda
+from Model.Pessoa import Pessoa
+from Model.Contato import Contato
+from Model.Telefone import *
 def menu():
-  print("""
-  1- Quantidade de contatos
-  2- Incluir contato
-  3- Apagar contato
-  4-Visualisar lista de contatos
-  5- Buscar contato
-  6- Sair""")
-  return regulariza("Escolha uma opção: ", 1, 6)
-while True:
-    op = menu()
-    if op == 6
-        break
+    def regulariza(pergunta, inicio, fim):
+        while True:
+            try:
+                valor = int(input(pergunta))
+                if inicio <= valor <= fim:
+                    return (valor)
+                return
+            except ValueError:
+                print("valor inválido, digite entre %d e %d" % (inicio, fim))
 
+    print("""
+  1- Adicionar novo contato
+  2- Pesquisar contato
+  3- Visualizar lista de contatos
+  4- Quantidade de Contatos
+  5- Apagar um contato
+  6- Sair""")
+    return regulariza("Escolha uma opção: ", 1, 6)
+
+
+while True:
+    opção = menu()
+    if opção == 6:
+        break
+    elif opção == 1:
+        Agenda.AdicionarNovoContato:()
+    elif opção == 2:
+        Agenda.PesquisarContato:()
+    elif opção == 3:
+        Agenda.ListarDados:()
+    elif opção == 4:
+        Agenda.QuantidadeDeContatos:()
+    elif opção == 5:
+        Agenda.ApagarUmContato:()
 
 def main():
-    while True:
-        try:
-            f = open("meses.txt", encoding="utf8")
-            linhas = f.readlines()
+    def nome():
+        return (input("Nome: "))
 
-            for linha in linhas:
-                print(linha.strip())
-
-            break
-
-        except FileNotFoundError:
-            print("arquivo não encontrado")
-               
-if __name__ == '__main__':
+    def telefone():
+        return (input("Telefone: "))
+if __name__=='__main__':
     main()
