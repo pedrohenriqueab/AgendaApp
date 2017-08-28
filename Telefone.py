3dd3c3a409d00989da79efcdcb3df6b71e76e3b2
MarinaAraujo <marinamarttinsmap@gmail.com>
2017-08-28 10:59:58 -0300

    Update Telefone.py

--- Telefone.py
+++ Telefone.py
@@ -1,14 +1,5 @@
-import datetime
 class Telefone(object):
     def __init__(self, codigoPais, ddd, numero):
         self.codigoPais = codigoPais
         self.ddd = ddd
         self.numero = numero
-      
-
-class Contato():
-    def __init__(self, criacao):
-        self.criacao = criacao(datetime.datetime.now().strftime("%y-%m-%d-%H-%M"))
-
-    def listarTelefone(self):
-        pass
