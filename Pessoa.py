0ebc1ef8de94ba1c63246c1baf5b4fabfc59bae2
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2017-08-28 14:45:04 -0300

    Update Pessoa.py

--- Pessoa.py
+++ Pessoa.py
@@ -2,8 +2,9 @@ import datetime
 class Pessoa(object):
     def main(self, nome, nascimento, email):
         self.nome = nome
-        self.nascimento = nascimento(datetime.date(2001, 10, 9))
-        self.email = email('proprietario@gmail.com')
+        self.nascimento = nascimento(datetime.date)
+        self.email = email
+
 
 
 
