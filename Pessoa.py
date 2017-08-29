65207f060e522fd1a0d57e5148534fc865b26b8a
MarinaAraujo <marinamarttinsmap@gmail.com>
2017-08-29 10:41:53 -0300

    Update Pessoa.py

--- Pessoa.py
+++ Pessoa.py
@@ -1,16 +1,8 @@
 import datetime
+
 class Pessoa(object):
+    
     def main(self, nome, nascimento, email):
         self.nome = nome
         self.nascimento = nascimento(datetime.date)
         self.email = email
-
-
-
-
-
-
-
-
-
-
